52522bf3fea52d708c7e06f525a6c16bb680b8c5
s4wo <p4ul@disroot.org>
2024-10-09 16:31:01 +0200

    Update 3.4.2.py

--- S1/INFO1/Chapitre_3/3.4.2.py
+++ S1/INFO1/Chapitre_3/3.4.2.py
@@ -1,5 +1,3 @@
-# h = list(range(24))
-# m = list(range(60))
 
 def uneMinuteEnPlus(h: int,m: int):
     h >= 0 and h >= 23
@@ -20,18 +18,35 @@ print(uneMinuteEnPlus(12,1))
 
       
 def uneMinuteEnPlus1(h: int,m: int):
-    h >= 0 and h >= 23
     if m > 59 or m < 0 or h > 23 or h < 0:
         return False
     if m >= 0 and m < 59:
         m = m + 1
         h = h
-    if m == 59:
-            m = 00
+    elif m == 59:
+            m = 0
             h = h + 1
     if h == 24:
         h = 00
     heure = (h,m)
+   
     return (heure)
 
-print(uneMinuteEnPlus1(12,1))
+print(uneMinuteEnPlus1(23,59))
+
+
+def uneMinuteEnPlus2(h: int,m: int):
+    mh = (h,m)
+    # h >= 0 and h >= 23
+    if m > 59 or m < 0 or h > 23 or h < 0:
+        return False
+    if h == 23 and m == 59:
+        mh = (0,0)
+    if mh == (h,59):
+        mh = (h+1,00)
+    elif mh == (h,m):
+        mh = (h,m+1)
+    return mh
+    
+print(uneMinuteEnPlus2(22,59))
+
