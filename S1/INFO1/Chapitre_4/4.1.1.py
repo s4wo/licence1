339d16bdb51f12703bbec446ce4145ecc6c25f05
s4wo <p4ul@disroot.org>
2024-10-16 15:52:26 +0200

    Update 4.1.1.py

--- S1/INFO1/Chapitre_4/4.1.1.py
+++ S1/INFO1/Chapitre_4/4.1.1.py
@@ -1,1 +1,11 @@
+L = [-5,2,9,-5,999,-7]
 
+
+def maximumListe(L: list):
+    s = L[0]
+    for elt in L: 
+        if elt > s:
+            s = elt
+    return s
+
+print(f"La valeur maximum de la liste est {maximumListe(L)}.")
