4da6915720ff8126997c3badf399077641d3547b
s4wo <p4ul@disroot.org>
2024-10-23 17:49:36 +0200

    Update 5.2.4.py

--- S1/INFO1/Chapitre_5/5.2.4.py
+++ S1/INFO1/Chapitre_5/5.2.4.py
@@ -1,1 +1,21 @@
+from bibimages import *
 
+monImage = nouvelleImage(300, 300)
+c = (255,100,0)
+d = 20
+
+def ligneHorizontale(img, c, y):
+    l = largeurImage(img)
+    h = hauteurImage(img)
+    for x in range(l):
+        colorierPixel(img, x, y, c)
+        
+def grilleHorizontale(img, c, d):
+    l = largeurImage(img)
+    h = hauteurImage(img)
+    y = 0 
+    for yo in range(0,h,d):
+        ligneHorizontale(img, c, yo)
+
+grilleHorizontale(monImage,c ,d)
+afficherImage(monImage)
