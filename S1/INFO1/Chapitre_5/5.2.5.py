08c057e1bc406186dec7b146a9f63260b380af65
s4wo <p4ul@disroot.org>
2024-10-23 18:18:58 +0200

    Update 5.2.5.py

--- S1/INFO1/Chapitre_5/5.2.5.py
+++ S1/INFO1/Chapitre_5/5.2.5.py
@@ -1,1 +1,14 @@
+from bibimages import *
 
+monImage = nouvelleImage(1000, 1000 )
+x1 = 50
+x2 = 100
+y1 = 150
+y2 = 200
+
+def ligneHorizontal(img: , x: int, y: int):
+    l = largeurImage(img)
+    h = hauteurImage(img)
+    for x in range(x)
+
+def rectangleCreux
