6447507e9f7cc8993dae2eee96e96e35181ece5c
s4wo <p4ul@disroot.org>
2024-10-23 17:12:25 +0200

    Update 5.2.2.py

--- S1/INFO1/Chapitre_5/5.2.2.py
+++ S1/INFO1/Chapitre_5/5.2.2.py
@@ -1,1 +1,13 @@
+from bibimages import *
 
+monImage = nouvelleImage(300, 300)
+c = (255,100,0)
+
+def ligneHorizontaleAuMilieu(monImage, c):
+    l = largeurImage(monImage)
+    h = hauteurImage(monImage)
+    for x in range(l):
+        colorierPixel(monImage, x, h//2, c)
+
+ligneHorizontaleAuMilieu(monImage, c)
+afficherImage(monImage)
