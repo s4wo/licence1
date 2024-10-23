ae8710097855e4d3438754192537de82df381c93
s4wo <p4ul@disroot.org>
2024-10-23 18:24:58 +0200

    Update 5.2.5.py

--- S1/INFO1/Chapitre_5/5.2.5.py
+++ S1/INFO1/Chapitre_5/5.2.5.py
@@ -5,10 +5,15 @@ x1 = 50
 x2 = 100
 y1 = 150
 y2 = 200
+c = (255.255.255)
 
-def ligneHorizontal(img: , x: int, y: int):
+def ligneHorizontal(img, x1: int, y1: int, x2: int, y2: int):
     l = largeurImage(img)
     h = hauteurImage(img)
-    for x in range(x)
+    for x in range(l):
+        colorierPixel(img, x2-x1, y2-y1, c)
+        
+ligneHorizontal(monImage,x1,y1,x2,y2)
+afficherImage(monImage)
 
-def rectangleCreux
+# def rectangleCreux
