683b86e6a48a1d2ef5f71a74e0ed30e65a884f4b
s4wo <p4ul@disroot.org>
2024-10-23 16:47:52 +0200

    Update 5.1.1.py

--- S1/INFO1/Chapitre_5/5.1.1.py
+++ S1/INFO1/Chapitre_5/5.1.1.py
@@ -1,1 +1,14 @@
+from bibimages import *
+
+
+monImage = nouvelleImage (300,200)
+colorierPixel(monImage, 100,100, (0,355,355))
+afficherImage(monImage)
+
+l = largeurImage(monImage)
+h = largeurImage(monImage)
+
+theiere = ouvrirImage ("teapot.png")
+afficherImage(theiere)
+
 
