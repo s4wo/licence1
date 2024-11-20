13719aec8313c9f14eb2adb530ac848f63ee8e2e
s4wo <p4ul@disroot.org>
2024-11-20 18:10:29 +0100

    Update 7.1.4_RandRange.py

--- S1/INFO1/Chapitre_7/7.1.4_RandRange.py
+++ S1/INFO1/Chapitre_7/7.1.4_RandRange.py
@@ -1,1 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from random import randrange
+
+
+def lancerDe():
+    return randrange(1,7)
+
+def obtenirUnNombre(n):
+    c = 0
+    while lancerDe() != n: 
+        c = c + 1
+    return c
+
+print(obtenirUnNombre(6))
+
+def obtenirUnDouble(n):
+    c = 0
+    lancerDe() = c
+    
 
