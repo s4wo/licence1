c0bdb11471e8d5de8e5b98001fd2e82ec18c27d9
s4wo <p4ul@disroot.org>
2024-11-20 17:45:34 +0100

    Update 7.1.1_Boucle_Conditionnelle.py

--- S1/INFO1/Chapitre_7/7.1.1_Boucle_Conditionnelle.py
+++ S1/INFO1/Chapitre_7/7.1.1_Boucle_Conditionnelle.py
@@ -1,1 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+placement = 100
+taux = 1
+
+def interets(placement: int, taux: int):
+    total = placement
+    years = 0
+    while total < placement*2 :
+        total = total + (total/100*taux)
+        years = years+1
+    return years
+
+print(interets(placement, taux))
