84643f9c34d1a8969e79186b470ef75ca3f17690
s4wo <p4ul@disroot.org>
2024-12-04 17:54:19 +0100

    Update 8.3.2_degreeMax.py

--- S1/INFO1/Chapitre_8/8.3.2_degreeMax.py
+++ S1/INFO1/Chapitre_8/8.3.2_degreeMax.py
@@ -1,1 +1,18 @@
+from bibgraphes import *
+
+G = ouvrirGraphe("tgv2005.dot")
+
+def degreeMax(G):
+    ville = ""
+    nbMax = 0
+    for a in listeSommets(G):
+        if degre(a) > nbMax:
+            nbMax = degre(a)
+            ville = nomSommet(a)
+    return ville, nbMax
+
+print(degreeMax(G))
+
+degre
+
 
