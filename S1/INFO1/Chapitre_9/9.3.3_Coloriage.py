d4e0ab9de3cb9879a86ce509f16b03d04ae71aa0
s4wo <p4ul@disroot.org>
2024-12-11 17:28:56 +0100

    Update 9.3.3_Coloriage.py

--- S1/INFO1/Chapitre_9/9.3.3_Coloriage.py
+++ S1/INFO1/Chapitre_9/9.3.3_Coloriage.py
@@ -1,1 +1,9 @@
+def contientDuBlanc(img: image, c):
+    resultat = True
+    for x in range(largeurImage(img)):
+        for y in range(hauteurImage(img)):
+            if couleurPixel(img, x, y) != c:
+                resultat = False
+    return resultat
 
+print(contientDuBlanc(img, c))
